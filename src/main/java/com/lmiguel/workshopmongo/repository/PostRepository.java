866018bd2d7da2d3130825214036210e41e30a34
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-05-27 11:54:57 -0300

    consulta simples com query methods

--- src/main/java/com/lmiguel/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lmiguel/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.lmiguel.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.lmiguel.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnooreCase(String text);
+	
 }
